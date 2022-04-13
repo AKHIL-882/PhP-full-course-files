11896a308a5e7c0cfe3c6a34732d84c7b4842e44
AKHIL-882 <51040667+AKHIL-882@users.noreply.github.com>
2022-04-13 12:05:58 +0530

    Add files via upload

---
+++ Student Management Portal/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>| Student Management Protal |</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <nav>
+        <label for="" class="logo"> RGUKT-LMS</label>
+        <ul>
+            <li><a href="http://">Home</a></li>
+            <li><a href="http://">Contact</a></li>
+            <li><a href="http://">Admissions</a></li>
+            <li><a href="http://" class="btn btn-success">Login</a></li>
+        </ul>
+    </nav>
+
+    <div class="section1">
+        <label for="" class="img_text"> WELCOME TO LEARNING MANAGEMENT SYSTEM </label>
+        <img src="images/2.jpg" alt="" class="main_img">
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="images/1.jpg" alt="" class="welcome_img">
+            </div>
+            <div class="col-md-8">
+                <h1> Key Insights</h1>
+                <p>
+                Rajiv Gandhi University of Knowledge Technologies (RGUKT) is a state university in Basar, Telangana, India. The university offers Undergraduate, Post Graduate and Ph.D degree in various technology programmes. The institute offers six-year Bachelor of Technology after 10th class examinations
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+    <center> <h1> Faculty </h1>  </center>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="images/1.jpg" alt="" class="teacher">
+                <p>Rajiv Gandhi University of Knowledge Technologies (RGUKT) is a state university in Basar, Telangana, India.</p>
+            </div>
+            <div class="col-md-4">
+                <img src="images/1.jpg" alt="" class="teacher">
+                <p>Rajiv Gandhi University of Knowledge Technologies (RGUKT) is a state university in Basar, Telangana, India.</p>
+            </div>
+            <div class="col-md-4">
+                <img src="images/1.jpg" alt="" class="teacher">
+                <p>Rajiv Gandhi University of Knowledge Technologies (RGUKT) is a state university in Basar, Telangana, India.</p>
+            </div>
+        </div>
+    </div>
+
+    <center> <h1> Courses </h1>  </center>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="images/2.jpg" alt="" class="teacher">
+                <h3>Operating System</h3>
+            </div>
+            <div class="col-md-4">
+                <img src="images/2.jpg" alt="" class="teacher">
+                <h3>Digital Logics</h3>
+            </div>
+            <div class="col-md-4">
+                <img src="images/2.jpg" alt="" class="teacher">
+                <h3>Compiler Design</h3>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <h5 class="footer_text">Copyrights @ Duggirala Akhil</h5>
+    </footer>
+
+</body>
+</html>
